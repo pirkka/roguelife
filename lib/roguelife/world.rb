2cfda3a0ad8fde522fbff821922b8ccb10d75d06
Pirkka Esko <pirkka@gmail.com>
2013-07-17 22:12:24 +0300

    generating some random content for map

--- lib/roguelife/world.rb
+++ lib/roguelife/world.rb
@@ -3,7 +3,7 @@ class World
   attr_accessor :tiles
   
   def get_height_map
-    [[1,2,3], [4,5,6], [7,8,9]]
+    return tiles
   end
     
 end
--- lib/roguelife/world_generator.rb
+++ lib/roguelife/world_generator.rb
@@ -1,11 +1,25 @@
 module WorldGenerator 
 
-  @@world_size = 50
-
+  @@world_height = 26
+  @@world_width = 90
+  
   def self.generate
     puts 'Generating world...'
     world = World.new
+    world.tiles = self.generate_tiles
     return world
+  end
+  
+  def self.generate_tiles
+    tiles = []
+    (1..@@world_height).each do 
+      row = []
+      (1..@@world_width).each do 
+        row << rand(5)
+      end
+      tiles << row
+    end
+    return tiles    
   end
 
 end
