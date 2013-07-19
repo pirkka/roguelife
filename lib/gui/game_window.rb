bf53c6e4905e4d8f7ed4e8763a9f012d6e7efa50
Pirkka Esko <pirkka@gmail.com>
2013-07-19 16:15:27 +0300

    tile size & window size settings

--- lib/gui/game_window.rb
+++ lib/gui/game_window.rb
@@ -1,12 +1,18 @@
 require 'gosu'
 
+module UISettings
+  TileSize = 25
+  WindowWidth = 800
+  WindowHeight = 800
+end
+
 module ZOrder
   Background, Tiles, Agents, Player, UI = *0..4
 end
 
 class GameWindow < Gosu::Window
   def initialize
-    super 800, 800, false
+    super UISettings::WindowWidth, UISettings::WindowHeight, false
     self.caption = "Roguelife"
     @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
   end
@@ -17,7 +23,7 @@ class GameWindow < Gosu::Window
   def draw
     @font.draw("@", 200, 200, ZOrder::UI, 1.0, 1.0, 0xffffff00)
     
-    draw_quad(0,0,0xFFFFFF00, 0,20, 0xFFFFFF00, 20,20, 0xFFFFFF00, 20,0, 0xFFFFFF00, 0)
+    draw_quad(0, 0, 0xFFFFFF00, 0, UISettings::TileSize, 0xFFFFFF00, UISettings::TileSize, UISettings::TileSize, 0xFFFFFF00, UISettings::TileSize, 0, 0xFFFFFF00, 0)
   end
   
   def button_down(id)
